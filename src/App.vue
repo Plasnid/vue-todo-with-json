f7eacb45543a9a997838b954dd48980671db98e7
Plasnid <stuart@sackler.ca>
2020-10-12 23:18:47 -0400

    data comes from json file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{showCompletion}}</h1>
+    <ToDo :todos="tasks" @comp-change="toDoStatus" @del-entry="deleteEntry"/>
+    <TaskForm :todos="tasks" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/Todo.vue'
+import TaskForm from './components/TaskForm.vue'
+import stuffToDo from '@/assets/data.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo,
+    TaskForm
+  },
+  data: function(){
+    return {
+      showCompletion: "",
+      tasks: stuffToDo.tasks
+    }
+  },
+  mounted: function(){
+    console.log(this.tasks);
+    this.tasks = stuffToDo.tasks;
+  },
+  methods: {
+    toDoStatus() {
+      let tasksDone = this.tasks.every(val => val.status==true);
+      if(tasksDone==true){
+        this.showCompletion = "All Done";
+      }else{
+        this.showCompletion = "More stuff to do!";
+      }
+    },
+    deleteEntry(key){
+      this.tasks.splice(key,1);
+    }
+  },
+  watch: {
+    tasks() {
+      this.toDoStatus();
+    },
+  },
+  created: function(){
+    /**
+     * otherhooks include beforeCreate, beforeMount, mounted, beforeUpdate, updated, beforeDestroy, and destroyed
+     * https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
+     */
+    this.toDoStatus();
   }
 }
 </script>
